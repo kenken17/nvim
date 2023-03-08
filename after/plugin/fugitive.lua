09e448a1e615a54b00195be32b6c4d9a3bdc8755
ken <kenken17@gmail.com>
2023-03-08 23:29:43 +0800

    Add in commit prev and next to mappings

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,6 +1,8 @@
 local opts = { noremap = true }
 local s_opts = { noremap = true, silent = true }
 
+vim.keymap.set("n", "<F7>", "[c", s_opts)
+vim.keymap.set("n", "<F8>", "]c", s_opts)
 vim.keymap.set("n", "<F9>", ":Git<Cr>:15wincmd_<Cr>", s_opts)
 vim.keymap.set("n", "<F10>", ":Gvdiffsplit<Space>", opts)
 vim.keymap.set("n", "<F11>", ":Git pull<Cr>", s_opts)
