503f29223e47b002579d6a6403e22972d6779629
ken <kenneth_lee@mom.gov.sg>
2023-04-04 02:37:02 +0800

    Remove the tmux split. buggy

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -17,7 +17,6 @@ map("n", "<F1>", ":help <C-r><C-w><Cr>", s_opts)
 -- Quit
 map("n", "<F2>", ":lua CloseOut()<Cr>", s_opts)
 map("n", "<Leader><F2>", ":tabclose<Cr>", s_opts)
-map("n", "<Leader><F12>", ":!tmux split-window -h git l<Cr><C-a>o", s_opts)
 
 -- Resize with arrows
 map("n", "<S-h>", ":vertical resize -2<Cr>", s_opts)
