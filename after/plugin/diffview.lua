b318aa3691c3a94182fd5589a8ad63b92c10a499
ken <kenneth_lee@mom.gov.sg>
2023-04-04 02:05:40 +0800

    Add in more mapping for diff view when resolve conflict

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -19,14 +19,14 @@ diffview.setup({
 			-- Config for conflicted files in diff views during a merge or rebase.
 			layout = "diff3_mixed",
 			disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
-			winbar_info = true, -- See |diffview-config-view.x.winbar_info|
+			winbar_info = true,      -- See |diffview-config-view.x.winbar_info|
 		},
 	},
 	file_panel = {
 		listing_style = "tree", -- One of 'list' or 'tree'
 		tree_options = {
 			-- Only applies when listing_style is 'tree'
-			flatten_dirs = false, -- Flatten dirs that only contain one single dir
+			flatten_dirs = false,         -- Flatten dirs that only contain one single dir
 			folder_statuses = "only_folded", -- One of 'never', 'only_folded' or 'always'.
 		},
 		win_config = {
@@ -49,11 +49,11 @@ diffview.setup({
 		view = {
 			-- The `view` bindings are active in the diff buffers, only when the current
 			-- tabpage is a Diffview.
-			{ "n", "<tab>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
-			{ "n", "<s-tab>", actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
-			{ "n", "<leader>e", actions.focus_files, { desc = "Bring focus to the file panel" } },
-			{ "n", "<leader>b", actions.toggle_files, { desc = "Toggle the file panel." } },
-			{ "n", "g<C-x>", actions.cycle_layout, { desc = "Cycle through available layouts." } },
+			{ "n", "<tab>",     actions.select_next_entry, { desc = "Open the diff for the next file" } },
+			{ "n", "<s-tab>",   actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
+			{ "n", "<Leader>e", actions.focus_files,       { desc = "Bring focus to the file panel" } },
+			{ "n", "<Leader>b", actions.toggle_files,      { desc = "Toggle the file panel." } },
+			{ "n", "g<C-x>",    actions.cycle_layout,      { desc = "Cycle through available layouts." } },
 			{
 				"n",
 				"<F5>",
@@ -66,16 +66,25 @@ diffview.setup({
 				actions.next_conflict,
 				{ desc = "In the merge-tool: jump to the next conflict" },
 			},
-			{ "n", "22", actions.conflict_choose("ours"), { desc = "Choose the OURS version of a conflict" } },
+			{ "n", "22",         actions.conflict_choose("ours"), { desc = "Choose the OURS version of a conflict" } },
+			{ "n", "<Leader>ll", actions.conflict_choose("ours"), { desc = "Choose the OURS version of a conflict" } },
 			{
 				"n",
 				"33",
 				actions.conflict_choose("theirs"),
 				{ desc = "Choose the THEIRS version of a conflict" },
 			},
-			{ "n", "11", actions.conflict_choose("base"), { desc = "Choose the BASE version of a conflict" } },
-			{ "n", "00", actions.conflict_choose("all"), { desc = "Choose all the versions of a conflict" } },
-			{ "n", "dx", actions.conflict_choose("none"), { desc = "Delete the conflict region" } },
+			{
+				"n",
+				"<Leader>rr",
+				actions.conflict_choose("theirs"),
+				{ desc = "Choose the THEIRS version of a conflict" },
+			},
+			{ "n", "11",         actions.conflict_choose("base"), { desc = "Choose the BASE version of a conflict" } },
+			{ "n", "<Leader>bb", actions.conflict_choose("base"), { desc = "Choose the BASE version of a conflict" } },
+			{ "n", "00",         actions.conflict_choose("all"),  { desc = "Choose all the versions of a conflict" } },
+			{ "n", "<Leader>aa", actions.conflict_choose("all"),  { desc = "Choose all the versions of a conflict" } },
+			{ "n", "dx",         actions.conflict_choose("none"), { desc = "Delete the conflict region" } },
 		},
 		diff1 = {
 			-- Mappings in single window diff layouts
@@ -88,14 +97,26 @@ diffview.setup({
 		diff3 = {
 			-- Mappings in 3-way diff layouts
 			{
-				{ "n", "x" },
+				{ "n",                                                            "x" },
 				"22",
 				actions.diffget("ours"),
 				{ desc = "Obtain the diff hunk from the OURS version of the file" },
 			},
 			{
-				{ "n", "x" },
+				{ "n",                                                            "x" },
+				"<Leader>ll",
+				actions.diffget("ours"),
+				{ desc = "Obtain the diff hunk from the OURS version of the file" },
+			},
+			{
+				{ "n",                                                              "x" },
 				"33",
+				actions.diffget("theirs"),
+				{ desc = "Obtain the diff hunk from the THEIRS version of the file" },
+			},
+			{
+				{ "n",                                                              "x" },
+				"<Leader>rr",
 				actions.diffget("theirs"),
 				{ desc = "Obtain the diff hunk from the THEIRS version of the file" },
 			},
@@ -104,20 +125,38 @@ diffview.setup({
 		diff4 = {
 			-- Mappings in 4-way diff layouts
 			{
-				{ "n", "x" },
+				{ "n",                                                            "x" },
 				"11",
 				actions.diffget("base"),
 				{ desc = "Obtain the diff hunk from the BASE version of the file" },
 			},
 			{
-				{ "n", "x" },
+				{ "n",                                                            "x" },
+				"<Leader>bb",
+				actions.diffget("base"),
+				{ desc = "Obtain the diff hunk from the BASE version of the file" },
+			},
+			{
+				{ "n",                                                            "x" },
 				"22",
 				actions.diffget("ours"),
 				{ desc = "Obtain the diff hunk from the OURS version of the file" },
 			},
 			{
-				{ "n", "x" },
+				{ "n",                                                            "x" },
+				"<Leader>ll",
+				actions.diffget("ours"),
+				{ desc = "Obtain the diff hunk from the OURS version of the file" },
+			},
+			{
+				{ "n",                                                              "x" },
 				"33",
+				actions.diffget("theirs"),
+				{ desc = "Obtain the diff hunk from the THEIRS version of the file" },
+			},
+			{
+				{ "n",                                                              "x" },
+				"<Leader>rr",
 				actions.diffget("theirs"),
 				{ desc = "Obtain the diff hunk from the THEIRS version of the file" },
 			},
@@ -128,7 +167,7 @@ diffview.setup({
 				actions.goto_file_edit()
 				vim.cmd("tabclose #")
 			end,
-			{ "n", "j", actions.next_entry, { desc = "Bring the cursor to the next file entry" } },
+			{ "n", "j",      actions.next_entry, { desc = "Bring the cursor to the next file entry" } },
 			{ "n", "<down>", actions.next_entry, { desc = "Bring the cursor to the next file entry" } },
 			{
 				"n",
@@ -142,7 +181,7 @@ diffview.setup({
 				actions.prev_entry,
 				{ desc = "Bring the cursor to the previous file entry." },
 			},
-			{ "n", "<cr>", actions.select_entry, { desc = "Open the diff for the selected entry." } },
+			{ "n", "<cr>",          actions.select_entry, { desc = "Open the diff for the selected entry." } },
 			{ "n", "<2-LeftMouse>", actions.select_entry, { desc = "Open the diff for the selected entry." } },
 			{
 				"n",
@@ -150,7 +189,7 @@ diffview.setup({
 				actions.toggle_stage_entry,
 				{ desc = "Stage / unstage the selected entry." },
 			},
-			{ "n", "S", actions.stage_all, { desc = "Stage all entries." } },
+			{ "n", "S", actions.stage_all,   { desc = "Stage all entries." } },
 			{ "n", "U", actions.unstage_all, { desc = "Unstage all entries." } },
 			{
 				"n",
@@ -194,12 +233,12 @@ diffview.setup({
 				actions.refresh_files,
 				{ desc = "Update stats and entries in the file list." },
 			},
-			{ "n", "<leader>e", actions.focus_files, { desc = "Bring focus to the file panel" } },
-			{ "n", "<leader>b", actions.toggle_files, { desc = "Toggle the file panel" } },
-			{ "n", "g<C-x>", actions.cycle_layout, { desc = "Cycle available layouts" } },
-			{ "n", "<F5>", actions.prev_conflict, { desc = "Go to the previous conflict" } },
-			{ "n", "<F6>", actions.next_conflict, { desc = "Go to the next conflict" } },
-			{ "n", "cc", ":tabclose<Cr>:Git commit<Cr>", { desc = "Commit the staged changes" } },
+			{ "n", "<Leader>e", actions.focus_files,            { desc = "Bring focus to the file panel" } },
+			{ "n", "<Leader>b", actions.toggle_files,           { desc = "Toggle the file panel" } },
+			{ "n", "g<C-x>",    actions.cycle_layout,           { desc = "Cycle available layouts" } },
+			{ "n", "<F5>",      actions.prev_conflict,          { desc = "Go to the previous conflict" } },
+			{ "n", "<F6>",      actions.next_conflict,          { desc = "Go to the next conflict" } },
+			{ "n", "cc",        ":tabclose<Cr>:Git commit<Cr>", { desc = "Commit the staged changes" } },
 			{
 				"n",
 				"ca",
@@ -213,15 +252,15 @@ diffview.setup({
 				actions.goto_file_edit()
 				vim.cmd("tabclose #")
 			end,
-			{ "n", "g!", actions.options, { desc = "Open the option panel" } },
+			{ "n", "g!", actions.options,         { desc = "Open the option panel" } },
 			{
 				"n",
 				"y",
 				actions.copy_hash,
 				{ desc = "Copy the commit hash of the entry under the cursor" },
 			},
-			{ "n", "L", actions.open_commit_log, { desc = "Show commit details" } },
-			{ "n", "zR", actions.open_all_folds, { desc = "Expand all folds" } },
+			{ "n", "L",  actions.open_commit_log, { desc = "Show commit details" } },
+			{ "n", "zR", actions.open_all_folds,  { desc = "Expand all folds" } },
 			{ "n", "zM", actions.close_all_folds, { desc = "Collapse all folds" } },
 			{
 				"n",
@@ -253,26 +292,26 @@ diffview.setup({
 				actions.select_entry,
 				{ desc = "Open the diff for the selected entry." },
 			},
-			{ "n", "<tab>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
+			{ "n", "<tab>",     actions.select_next_entry,          { desc = "Open the diff for the next file" } },
 			{
 				"n",
 				"<s-tab>",
 				actions.select_prev_entry,
 				{ desc = "Open the diff for the previous file" },
 			},
-			{ "n", "<leader>e", actions.focus_files, { desc = "Bring focus to the file panel" } },
-			{ "n", "<leader>b", actions.toggle_files, { desc = "Toggle the file panel" } },
-			{ "n", "g<C-x>", actions.cycle_layout, { desc = "Cycle available layouts" } },
-			{ "n", "g?", actions.help("file_history_panel"), { desc = "Open the help panel" } },
+			{ "n", "<Leader>e", actions.focus_files,                { desc = "Bring focus to the file panel" } },
+			{ "n", "<Leader>b", actions.toggle_files,               { desc = "Toggle the file panel" } },
+			{ "n", "g<C-x>",    actions.cycle_layout,               { desc = "Cycle available layouts" } },
+			{ "n", "g?",        actions.help("file_history_panel"), { desc = "Open the help panel" } },
 		},
 		option_panel = {
-			{ "n", "<tab>", actions.select_entry, { desc = "Change the current option" } },
-			{ "n", "q", actions.close, { desc = "Close the panel" } },
-			{ "n", "R", actions.refresh_files, { desc = "Update stats and entries in the file list." } },
-			{ "n", "g?", actions.help("option_panel"), { desc = "Open the help panel" } },
+			{ "n", "<tab>", actions.select_entry,         { desc = "Change the current option" } },
+			{ "n", "q",     actions.close,                { desc = "Close the panel" } },
+			{ "n", "R",     actions.refresh_files,        { desc = "Update stats and entries in the file list." } },
+			{ "n", "g?",    actions.help("option_panel"), { desc = "Open the help panel" } },
 		},
 		help_panel = {
-			{ "n", "q", actions.close, { desc = "Close help menu" } },
+			{ "n", "q",     actions.close, { desc = "Close help menu" } },
 			{ "n", "<esc>", actions.close, { desc = "Close help menu" } },
 		},
 	},
