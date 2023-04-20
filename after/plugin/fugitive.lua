7de6c796ace415f202730f09e31be5e31d845b51
ken <kenneth_lee@mom.gov.sg>
2023-04-20 14:17:07 +0800

    Revert the echo. Not working

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,9 +1,8 @@
-local opts = { noremap = true }
 local s_opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "<F7>", "[c", s_opts)
 vim.keymap.set("n", "<F8>", "]c", s_opts)
 vim.keymap.set("n", "<Leader><F9>", ":Git blame --date=relative -w<Cr>", s_opts)
-vim.keymap.set("n", "<F11>", ':echo "Pulling..."<CR>:Git pull<Cr>', opts)
-vim.keymap.set("n", "<Leader><F11>", ':echo "Pushing..."<CR>:Git push<Cr>', opts)
+vim.keymap.set("n", "<F11>", ":Git pull<Cr>", s_opts)
+vim.keymap.set("n", "<Leader><F11>", ":Git push<Cr>", s_opts)
 -- vim.keymap.set("n", "<Leader><F11>", ":Git push --force-with-lease<Cr>", s_opts)
